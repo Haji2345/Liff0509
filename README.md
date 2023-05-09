5305ade0b79576844e170d90d72f928625e803a0
Haji2345 <132870728+Haji2345@users.noreply.github.com>
2023-05-09 10:08:10 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Liff0509
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-m4hghl)
---
+++ index.js
@@ -0,0 +1,21 @@
+// Import stylesheets
+import "./style.css"
+
+// Body element
+
+
+// Button elements
+
+
+// Profile elements
+
+
+// QR element
+
+
+async function main() {
+  // Initialize LIFF app)
+  liff.init({ liffId: "1661054856-nPEmwyoR" })
+  // Try a LIFF function
+}
+main()
